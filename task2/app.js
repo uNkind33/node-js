a7f982f9b7b4b915e293286c2c0090e8f2abc864
Oleksandr Moiseiev <Oleksandr_Moiseiev@epam.com>
2018-02-21 21:10:13 +0200

    added methods and files

--- task2/app.js
+++ task2/app.js
@@ -9,15 +9,9 @@ new http.Server((req, res) => {
         })
         const file = new fs.createReadStream(__dirname + '/public/index.html', 'utf8');
         file.pipe(res)
-        // var filename = path.basename('../files/file.txt');
-
-        // sendFile(file, res);
 
     } else if (req.url === '/file.txt' && req.method === "GET") {
-        console.log(req.url)
         var filePath = req.url.substr(1);
-        console.log(filePath)
-        // res.writeHead(404, 'Not found');
         try {
             fs.statSync(__dirname + '/files/file.txt');
             res.writeHead(200, {
@@ -28,26 +22,18 @@ new http.Server((req, res) => {
         } catch (err) {
             res.statusCode = 404;
             res.end('Page not found')
-
         }
-
-        // if (fs.statSync(__dirname + '/files/file.txt'), err) {
-        //     res.writeHead(200, {
-        //         'Content-Type': 'text/plain'
-        //     })
-        //     const file = new fs.createReadStream(__dirname + '/files/file.txt', 'utf8');
-        //     file.pipe(res)
-        // } else {
-        //     res.writeHead(404, {
-        //         'Content-Type': 'text/plain'
-        //     })
-        //    res.end('Page not found')
-        // }
     } else if (req.url === '/file.txt' && req.method === "POST") {
         res.writeHead(200, {
             'Content-Type': 'text/plain'
         })
         const file = new fs.createWriteStream(__dirname + '/files/file.txt', 'utf8');
+        fs.exists('./files/file.txt', function (exists) {
+            if (exists) {
+                res.statusCode = 409;
+                res.end('File exits')
+            }
+        })
         let data = "Hello world";
         file.write(data, err => {
             if (err) {
@@ -56,29 +42,17 @@ new http.Server((req, res) => {
                 console.log('written');
             }
         })
+
+    } else if (req.url === '/file.txt' && req.method === "DELETE") {
+        fs.exists('./files/file.txt', function (exists) {
+            if (exists) {
+                fs.unlink('./files/file.txt');
+                res.status = 200;
+                res.end("Successfully deleted")
+            } else {
+                console.log('File not found, so not deleting.');
+            }
+        });
     }
 
-    // if (req.url == '/test') {
-    //     console.log(req.url)
-    //     let file = new fs.ReadStream('file.txt', {encoding: 'utf-8'});
-    //     sendFile(file, res);
-    // }
-
-    // if( req.url == '/file') {
-
-    // }
 }).listen(3000);
-
-function sendFile(file, res) {
-    file.pipe(res)
-    file.on('error', function (err) {
-        res.statusCode = 500;
-        res.end('Server Error');
-        console.log(err)
-    })
-    // file.on('data', chunk => {     res.write(chunk); }); file.on('end', () => {
-    // res.end(); });
-}
-
-// function sendFile(file, res) {     file.on('data', chunk => {
-// res.write(chunk);     });     file.on('end', () => {         res.end(); }); }
