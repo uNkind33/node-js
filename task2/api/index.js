2c6099ae5a0ae5397c0569e2a20af7aa86c22f31
Alexander Moiseev <unkind33@gmail.com>
2018-02-26 03:07:41 +0200

    moved api into separate directory

---
+++ task2/api/index.js
@@ -0,0 +1,74 @@
+let fs = require('fs');
+
+module.exports = {
+    getMainPage(res, req) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        const file = new fs.createReadStream('./public/index.html', 'utf8');
+        file.pipe(res)
+    },
+
+    getFile(res, req) {
+        let filePath = req.url.substr(1);
+        try {
+            fs.statSync(`./files/${filePath}`);
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            })
+            const file = new fs.createReadStream(`./files/${filePath}`, 'utf8');
+            file.pipe(res)
+        } catch (err) {
+            res.statusCode = 404;
+            res.end('Page not found')
+        }
+    },
+
+    postFile(res, req) {
+        let filePath = req.url.substr(1);
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        })
+        const file = new fs.createWriteStream(`./files/${filePath}`, 'utf8');
+        fs.exists(`./files/${filePath}`, function (exists) {
+            if (exists) {
+                res.statusCode = 409;
+                res.end('File exits')
+            }
+        })
+        data = "Hello World"
+        let dataLength = 0;
+        file.on('data', function (chunk) {
+            if (dataLength > 1e6) {
+                file.destroy()
+            }
+            dataLength += chunk.length;
+          })
+
+        file.write(data, err => {
+            if (err) {
+                console.log(err.message)
+            } else {
+                console.log('written');
+            }
+        })
+    },
+
+    deleteFile(res, req) {
+        let filePath = req.url.substr(1);
+        fs.exists(`./files/${filePath}`, function (exists) {
+            console.log(exists)
+            if (exists) {
+                fs.unlink(`./files/${filePath}`, function (err) {
+                    if (err) {
+                        console.log(err);
+                    };
+                    res.end('file deleted');
+                });
+            } else {
+                res.statusCode = 404;
+                res.end('Page not found')
+            }
+        });
+    }
+}
--- task2/app.js
+++ task2/app.js
@@ -1,65 +1,21 @@
 let http = require('http');
 let fs = require('fs');
 let path = require('path');
+const ServerApi = require('./api')
 
 new http.Server((req, res) => {
     if (req.url === '/' && req.method === "GET") {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        })
-        const file = new fs.createReadStream(__dirname + '/public/index.html', 'utf8');
-        file.pipe(res)
+        ServerApi.getMainPage(res, req)
 
-    } else if (req.url === '/file.txt' && req.method === "GET") {
-        let filePath = req.url.substr(1);
-        try {
-            fs.statSync(__dirname + `/files/${filePath}`);
-            res.writeHead(200, {
-                'Content-Type': 'text/plain'
-            })
-            const file = new fs.createReadStream(__dirname + `/files/${filePath}`, 'utf8');
-            file.pipe(res)
-        } catch (err) {
-            res.statusCode = 404;
-            res.end('Page not found')
-        }
-    } else if (req.url === '/file.txt' && req.method === "POST") {
-        let filePath = req.url.substr(1);
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        })
-        const file = new fs.createWriteStream(__dirname + `/files/${filePath}`, 'utf8');
-        fs.exists(`./files/${filePath}`, function (exists) {
-            if (exists) {
-                res.statusCode = 409;
-                res.end('File exits')
-            }
-        })
-        let data = "Hello world";
-        file.write(data, err => {
-            if (err) {
-                console.log(err.message)
-            } else {
-                console.log('written');
-            }
-        })
+    } else if (req.method === "GET") {
+        ServerApi.getFile(res, req)
 
-    } else if (req.url === '/file.txt' && req.method === "DELETE") {
-        let filePath = req.url.substr(1);
-        fs.exists(`./files/${filePath}`, function (exists) {
-            console.log(exists)
-            if (exists) {
-                fs.unlink(`./files/${filePath}`, function (err) {
-                    if (err) {
-                        console.log(err);
-                    };
-                    res.end('file deleted');
-                });
-            } else {
-                res.statusCode = 404;
-                res.end('Page not found')
-            }
-        });
+    } else if (req.method === "POST") {
+        ServerApi.postFile(res, req);
+
+    } else if (req.method === "DELETE") {
+        ServerApi.deleteFile(res, req);
+
     } else {
         res.statusCode = 500;
         res.end('Server Error')
